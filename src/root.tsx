f7bbd670b122f8fdc879df52ace8378145a3e850
Alex Lohr <alex.lohr@logmein.com>
2023-04-01 22:39:37 +0200

    package updates

--- src/root.tsx
+++ src/root.tsx
@@ -1,24 +1,34 @@
 // @refresh reload
 import { Suspense } from "solid-js";
-import { A, Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title } from "solid-start";
+import {
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title
+} from "solid-start";
 import "./root.css";
 
 export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With MDX</Title>
+        <Title>Solid Cheat Sheet</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
-        <ErrorBoundary>
-          <Suspense>
+        <Suspense>
+          <ErrorBoundary>
             <Routes>
               <FileRoutes />
             </Routes>
-          </Suspense>
-        </ErrorBoundary>
+          </ErrorBoundary>
+        </Suspense>
         <Scripts />
       </Body>
     </Html>
