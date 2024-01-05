2866b50c04f3184d04a58e86f139e6610215a48a
atk <alexthkloss@web.de>
2024-01-05 10:24:06 +0000

    Deploying to gh-pages from @ atk/solid-cheat-sheet@cfae48a42d525762d98ed1f75c90c3a33e39db8c 🚀

---
+++ _build/assets/_...404_-068380c4.js
@@ -0,0 +1,3 @@
+import{M as r}from"./index-206de980.js";import{h as n,q as s}from"./web-cc7aa021.js";import{H as a}from"./HttpStatusCode-b5874230.js";function o(t){const e=Object.assign({h1:"h1",p:"p",a:"a"},r(),t.components);return[n(a,{code:404}),`
+`,n(e.h1,{children:"Page Not Found"}),`
+`,n(e.p,{get children(){return["Visit ",n(e.a,{href:"https://solidjs.com",children:"https://solidjs.com"})," to learn how to build Solid apps."]}})]}function m(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n(e,s(t,{get children(){return n(o,t)}})):o(t)}export{m as default};
