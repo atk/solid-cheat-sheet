3e537b0ef2027374bc1fd9ebc8ab629d8fba291b
Alex Lohr <alex.lohr@logmein.com>
2024-01-11 13:22:22 +0100

    fix router base prop

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <MetaProvider>
       <Router
-        base={process.env.BASE_PATH}
+        base={import.meta.env.SERVER_BASE_URL}
         root={(props) => (<Suspense>{props.children}</Suspense>)}
       >
         <FileRoutes />
